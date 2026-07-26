166e953f0a79b4819b13b5652951e329f191d44a
diyaghazal <diyaghazal932@gmail.com>
2026-07-26 16:55:09 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,17 @@
-public class Main{
-    public static void main(String args[]){
-        Calculator casio = new Calculator(10,4);
-        casio.add();
-        casio.show();
-        casio.subtract();
-        casio.show();
-        casio.multiply();
-        casio.show();
-        casio.divide();
-        casio.show();
-        casio.modulus();
-        casio.show();
-        casio.power();
-        casio.show();
-        casio.set(8,0);
-        casio.divide();
-        casio.show();
-        casio.set(6,2);
-        casio.average();
-        casio.show();
+public class Main {
+    public static void main(String[] args){
+        Car bmw = new Car();
+        bmw.startEngine();
+        bmw.setDetails("BMW" , "Black");
+        bmw.startEngine();
+        bmw.moveFast();
+        bmw.moveFast();
+        bmw.drive(50);
+        bmw.turboMode();
+        bmw.toggleHeadlights();
+        bmw.displayState();
+        System.out.println("Brand: " + bmw.getBrand()+ "Speed" + ", Speed: " + bmw.getSpeed()+ ",FuelLevel: " + bmw.getFuelLevel());
+        System.out.println();
     }
+    
 }
