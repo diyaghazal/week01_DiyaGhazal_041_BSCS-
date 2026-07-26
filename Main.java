c0356988129fb8303ea49e51bdd795775e229a46
diyaghazal <diyaghazal932@gmail.com>
2026-07-26 17:23:07 +0500

    Car Main.java

--- Main.java
+++ Main.java
@@ -15,25 +15,6 @@ public class Main {
     }
     
 }
-public class Main{
-    public static void main(String[] args){
-        Calculator casio = new Calculator();
-        casio.add();
-        casio.show();
-        casio.subtract();
-        casio.show();
-        casio.multiply();
-        casio.show();
-        casio.divide();
-        casio.show();
-        casio.modulus();
-        casio.show();
-        casio.power();
-        casio.show();
-        casio.set(8,0);
-        casio.divide();
-        casio.show();
-        casio.set(,);
-        casio.average();
-        casio.show();
+
+      
         
