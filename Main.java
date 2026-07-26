de89626d6d7162a58788f41b84ec1cca4a00f7e9
diyaghazal <diyaghazal932@gmail.com>
2026-07-26 17:33:54 +0500

    Main.java

--- Main.java
+++ Main.java
@@ -15,6 +15,26 @@ public class Main {
     }
     
 }
-
-      
+public class Main{
+    public static void main(String[] args){
+        Calculator casio =  new Calculator();
+        casio.add();
+        casio.show();
+        casio.subtract();
+        casio.show();
+        casio.multiply();
+        casio.show();
+        casio.divide();
+        casio.show();
+        casio.modulus();
+        casio.show();
+      casio.power();
+        casio.show();
+        casio.set(8,0);
+        casio.divide();
+        casio.show();
+        casio.set(6,2);
+        casio.average();
+        casio.show()
+        
         
